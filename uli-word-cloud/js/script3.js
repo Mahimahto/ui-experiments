dc224093dbf7bbec4b9d0ab7627294111dd95260
Manisha Kumari <manisha.tkg08@gmail.com>
2024-11-28 12:38:46 +0530

    Create divs on html

--- uli-word-cloud/js/script3.js
+++ uli-word-cloud/js/script3.js
@@ -1,24 +1,25 @@
-const mainContainer = document.querySelector("#main-container");
-let isDragging = false; 
+const dragingDiv = document.querySelector("#dragingDiv");
+let isDragging = false;
 
-mainContainer.addEventListener("mousedown", (e) => {
-    isDragging = true; 
+dragingDiv.addEventListener("mousedown", (e) => {
+    isDragging = true;
 });
 
-let width = parseInt(mainContainer.style.width)
-let height = parseInt(mainContainer.style.height)
+let width = parseInt(dragingDiv.style.width)
+let height = parseInt(dragingDiv.style.height)
 // console.log(height/2);
 // console.log(width/2)
 
 document.addEventListener("mousemove", (e) => {
-    if (isDragging) { 
+    if (isDragging) {
+        dragingDiv.style.position = "absolute";
         const x = e.clientX;
         const y = e.clientY;
-        mainContainer.style.left = `${x - width/2}px`;
-        mainContainer.style.top = `${y - width/2}px`;
+        dragingDiv.style.left = `${x - width / 2}px`;
+        dragingDiv.style.top = `${y - width / 2}px`;
     }
 });
 
 document.addEventListener("mouseup", () => {
-    isDragging = false; 
+    isDragging = false;
 });
